d673893cb41aee84e6407ff2fcc7d2f70d44a80d
victorjourne <journe.victor@gmail.com>
2023-11-23 12:43:11 +0000

    Change chunk size

--- app.py
+++ app.py
@@ -22,8 +22,10 @@ def get_pdf_text(pdf_docs):
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
+#        chunk_size=1000,
+#        chunk_overlap=200,
+        chunk_size=100,
+        chunk_overlap=5,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
